a999a3b3792a383f0a62a36a9bf3b2d092975e81
Jacques KOZIK <jacques.kzk@gmail.com>
2024-04-09 09:31:12 +0200

    COMMIT: 09/04/2024

--- Tableau.java
+++ Tableau.java
@@ -75,20 +75,14 @@ public class Tableau{
         }
     }
 
-<<<<<<< HEAD
     /*ajouterTentative, methode qui ajoute une tentative au tableau de tentative */
-=======
-    //ajouterTentative, methode qui ajoute une tentative au tableau de tentative
->>>>>>> refs/remotes/origin/main
     public void ajouterTentative(ArrayList<Pion> tentative){
         tableauTentative.add(tentative);
         tentativeActuelle++;
     }
-<<<<<<< HEAD
+
     /* verifierTentative, methode qui verifie si la tentative correspond au resultat attendu */
-=======
-    //verifierTentative, methode qui verifie si la tentative correspond au resultat attendu
->>>>>>> refs/remotes/origin/main
+
     public boolean verifierTentative(ArrayList<Pion> tentative){
         int bienPlace = this.getNombreCorrect(tentative);
         if(bienPlace == nombrePion){
@@ -99,7 +93,6 @@ public class Tableau{
         }
     }
 
-<<<<<<< HEAD
     public int getNombreCorrect(ArrayList<Pion> tentative){
         int bienPlace = 0;
         for(int i=0;i<nombrePion;i++){
@@ -110,13 +103,8 @@ public class Tableau{
         return bienPlace;
     }
 
-=======
-<<<<<<< HEAD
-    /* affichage, methode qui affiche le tableau de tentative */
-=======
->>>>>>> 3899f6560299f6d6bbd26518a6369a730ac1d9ab
+
     //affichage, methode qui affiche le tableau de tentative
->>>>>>> refs/remotes/origin/main
     public void affichage(){
         for(int i = 0 ; i < (nombreTentative - tentativeActuelle) ; i++){
                     String ligne = "[";
