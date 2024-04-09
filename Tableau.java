d08ad7350454fbb92a7da13c8d1e2528cd65e43f
Jacques KOZIK <jacques.kzk@gmail.com>
2024-04-09 09:11:39 +0200

    COMMIT: 09/04/2024

--- Tableau.java
+++ Tableau.java
@@ -1,17 +1,78 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
 
 public class Tableau{
+    Scanner input = new Scanner(System.in);
     private ArrayList<Pion> ligneDeviner; //tableau stockant la ligne de pion à deviner
     private ArrayList<ArrayList<Pion>> tableauTentative; //tableau stockant les tentatives
     private int nombreTentative;
     private int nombrePion;
     private int tentativeActuelle = 0;
+    private int nombreCouleur;
 
-    public Tableau(ArrayList<Pion> ligne, int T, int P) {
-        this.ligneDeviner = ligne;
+    public Tableau(boolean machine) {
+
         this.tableauTentative = new ArrayList<ArrayList<Pion>>();
-        this.nombreTentative = T;
-        this.nombrePion = P;
+
+        System.out.println("Bienvenue dans le jeu Mastermind !");
+
+        System.out.println("Combien de tentatives voulez-vous ?(10 ou 12)");
+        this.nombreTentative = input.nextInt();
+           
+        System.out.println("Combien de pions voulez-vous ?(4 ou 5)");
+        this.nombrePion = input.nextInt();
+
+        System.out.println("Combien de couleurs voulez-vous ?(6 ou 8)");
+        this.nombreCouleur = input.nextInt();
+
+        if(machine){
+            this.ligneDeviner = new ArrayList<Pion>();
+            for(int i=0;i<nombrePion;i++){
+                Random rand = new Random();
+                int couleur = rand.nextInt(nombreCouleur);
+                switch(couleur){
+                    case 0:
+                        this.ligneDeviner.add(new Pion(Couleur.ROUGE));
+                        break;
+                    case 1:
+                        this.ligneDeviner.add(new Pion(Couleur.JAUNE));
+                        break;
+                    case 2:
+                        this.ligneDeviner.add(new Pion(Couleur.VERT));
+                        break;
+                    case 3:
+                        this.ligneDeviner.add(new Pion(Couleur.BLEU));
+                        break;
+                    case 4:
+                        this.ligneDeviner.add(new Pion(Couleur.CYAN));
+                        break;
+                    case 5:
+                        this.ligneDeviner.add(new Pion(Couleur.ROSE));
+                        break;
+                    case 6:
+                        this.ligneDeviner.add(new Pion(Couleur.VIOLET));
+                        break;
+                    case 7:
+                        this.ligneDeviner.add(new Pion(Couleur.BLANC));
+                        break;
+                }
+            }
+        }
+        else{
+            System.out.println("Entrez la ligne à deviner : ");
+            this.ligneDeviner = new ArrayList<Pion>();
+            for(int i=0;i<nombrePion;i++){
+                if(nombreCouleur == 6){
+                    System.out.println("Il y a les couleurs suivantes: ROUGE, JAUNE, VERT, BLEU, CYAN, ROSE");
+                }
+                if(nombreCouleur == 8){
+                    System.out.println("Il y a les couleurs suivantes: ROUGE, JAUNE, VERT, BLEU, CYAN, ROSE, VIOLET, BLANC");
+                }
+                System.out.println("Entrez la couleur du pion " + (i+1) + " : ");
+                this.ligneDeviner.add(new Pion(Couleur.valueOf(input.nextLine())));
+            }
+        }
     }
 
     //ajouterTentative, methode qui ajoute une tentative au tableau de tentative
@@ -21,26 +82,23 @@ public class Tableau{
     }
     //verifierTentative, methode qui verifie si la tentative correspond au resultat attendu
     public boolean verifierTentative(ArrayList<Pion> tentative){
-        int bienPlace = 0;
-        int malPlace = 0;
-        for(int i=0;i<nombrePion;i++){
-            if(tentative.get(i).equals(ligneDeviner.get(i))){
-                bienPlace++;
-            }
-            else{
-                for(int j=0;j<nombrePion;j++){
-                    if(tentative.get(i).equals(ligneDeviner.get(j))){
-                        malPlace++;
-                    }
-                }
-            }
-        }
+        int bienPlace = this.getNombreCorrect(tentative);
         if(bienPlace == nombrePion){
             return true;
         }
         else{
             return false;
         }
+    }
+
+    public int getNombreCorrect(ArrayList<Pion> tentative){
+        int bienPlace = 0;
+        for(int i=0;i<nombrePion;i++){
+            if(tentative.get(i).equals(ligneDeviner.get(i))){
+                bienPlace++;
+            }
+        }
+        return bienPlace;
     }
 
     //affichage, methode qui affiche le tableau de tentative
@@ -53,7 +111,8 @@ public class Tableau{
                     ligne+= " ; ";
                 }                
             }
-            ligne+="]";
+            ligne+="] ";
+            ligne+="Bien placé : " + getNombreCorrect(tableauTentative.get(i));
             System.out.println(ligne);
         }
 
@@ -67,6 +126,35 @@ public class Tableau{
             }
             ligne+="]";
             System.out.println(ligne);
+        }
+    }
+
+    //debutPartie, methode qui lance une partie
+    public void debutPartie(){
+        boolean fin = false;
+        while(!fin){
+            System.out.println("Entrez votre tentative : ");
+            ArrayList<Pion> tentative = new ArrayList<Pion>();
+            for(int i=0;i<nombrePion;i++){
+                if(nombreCouleur == 6){
+                    System.out.println("Il y a les couleurs suivantes: ROUGE, JAUNE, VERT, BLEU, CYAN, ROSE");
+                }
+                if(nombreCouleur == 8){
+                    System.out.println("Il y a les couleurs suivantes: ROUGE, JAUNE, VERT, BLEU, CYAN, ROSE, VIOLET, BLANC");
+                }
+                System.out.println("Entrez la couleur du pion " + (i+1) + " : ");
+                tentative.add(new Pion(Couleur.valueOf(input.next())));
+            }
+            ajouterTentative(tentative);
+            affichage();
+            if(verifierTentative(tentative)){
+                System.out.println("Bravo, vous avez gagné !");
+                fin = true;
+            }
+            else if(tentativeActuelle == nombreTentative){
+                System.out.println("Désolé, vous avez perdu !");
+                fin = true;
+            }
         }
     }
 }
